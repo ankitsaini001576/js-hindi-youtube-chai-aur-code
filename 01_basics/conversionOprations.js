1409c6104ab4c94597285a26d2998838ef44ffdf
ankitsaini001576 <ankit.saini782005@gmail.com>
2026-03-27 21:05:37 +0530

    conversion

---
+++ 01_basics/conversionOprations.js
@@ -0,0 +1,33 @@
+//CONVERSION IN NUMBERS
+let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => when we convert this sting into number. 33
+//"33abc" => NaN
+//"true"=> 1; "false" => 0
+//undefiend => NaN
+
+// CONVERSION  IN BOOLEAN
+let isLoggedIn = 1
+let booleanIsLoggesIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggesIn);
+
+//1=> true; 0 => false
+//"" => false
+//"Ankit" => true
+
+//CONVERSION IN STRING
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
